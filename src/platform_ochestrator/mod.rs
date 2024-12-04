d019703fb55ea67acc88f6171565132e4bf0f0e5
joel-medicala-yral <joel@gobazzinga.io>
2024-12-04 17:11:13 +0530

    Remove test platform orchestrator

--- src/platform_ochestrator/mod.rs
+++ src/platform_ochestrator/mod.rs
@@ -1,3 +1,2 @@
 pub mod backup_restore;
-// pub const PLATFORM_ORCHESTRATOR_CANS_ID: &str = "74zq4-iqaaa-aaaam-ab53a-cai";
-pub const PLATFORM_ORCHESTRATOR_CANS_ID: &str = "bw4dl-smaaa-aaaaa-qaacq-cai";
+pub const PLATFORM_ORCHESTRATOR_CANS_ID: &str = "74zq4-iqaaa-aaaam-ab53a-cai";
