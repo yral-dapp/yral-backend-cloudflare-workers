f888472573e76b675e97edeed88954713a505110
tushar-saxena-yral <tushar@gobazzinga.io>
2024-12-23 23:59:17 +0530

    WIP: convert posts into work items

--- workers/storj-bulk-upload/src/lib.rs
+++ workers/storj-bulk-upload/src/lib.rs
@@ -1,24 +1,36 @@
 mod admin;
 mod posts;
 
+use std::sync::Arc;
+
 use admin::AdminCanisters;
-use futures::StreamExt;
-use worker::{event, Context, Env, Request, Response, Result};
+use anyhow::Context;
+use futures::TryStreamExt;
+use worker::{console_error, event, Context as WorkerContext, Env, Request, Response, Result};
 
 #[event(start)]
 fn start() {
     console_error_panic_hook::set_once();
 }
 
-#[event(fetch)]
-async fn fetch(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+#[event(fetch, respond_with_errors)]
+async fn fetch(_req: Request, _env: Env, _ctx: WorkerContext) -> Result<Response> {
     let admin = AdminCanisters::new(AdminCanisters::get_identity());
 
-    let items: Vec<_> = posts::load_items(&admin)
+    let items: anyhow::Result<Vec<_>> = posts::load_items(Arc::new(admin))
         .await
         .expect("TODO: handle error when items can't be loaded")
-        .collect()
-        .await;
+        .try_collect()
+        .await
+        .context("Couldn't load items");
+
+    let items = match items {
+        Ok(items) => items,
+        Err(err) => {
+            console_error!("{err}");
+            return Response::error("Failed to load items", 500);
+        }
+    };
 
     Response::from_json(&items)
 }
--- workers/storj-bulk-upload/src/posts.rs
+++ workers/storj-bulk-upload/src/posts.rs
@@ -1,5 +1,10 @@
+use std::sync::Arc;
+
 use anyhow::Context;
 use futures::{future, stream, StreamExt, TryStreamExt};
+use yral_canisters_client::individual_user_template::{
+    GetPostsOfUserProfileError, IndividualUserTemplate, PostDetailsForFrontend,
+};
 
 use crate::admin::AdminCanisters;
 
@@ -10,9 +15,35 @@ pub(crate) struct Item {
     // TODO: extra metadata
 }
 
-pub(crate) async fn load_items(
-    admin: &AdminCanisters,
-) -> anyhow::Result<impl futures::Stream<Item = Item>> {
+/// loads all posts for the given user and buffers into a vec before returning
+async fn load_all_posts(
+    user: &IndividualUserTemplate<'_>,
+) -> anyhow::Result<Vec<PostDetailsForFrontend>> {
+    const LIMIT: u64 = 100;
+    let mut posts = Vec::new();
+
+    for page in 0.. {
+        let post_res = user
+            .get_posts_of_this_user_profile_with_pagination_cursor(page, LIMIT)
+            .await
+            .context("Couldn't get post")?;
+
+        use yral_canisters_client::individual_user_template::Result12;
+        let post = match post_res {
+            Result12::Ok(posts) => posts,
+            Result12::Err(GetPostsOfUserProfileError::ReachedEndOfItemsList) => break,
+            Result12::Err(err) => anyhow::bail!("{err:?}"),
+        };
+
+        posts.extend(post.into_iter())
+    }
+
+    Ok(posts)
+}
+
+pub(crate) async fn load_items<'a>(
+    admin: Arc<AdminCanisters>,
+) -> anyhow::Result<impl futures::Stream<Item = anyhow::Result<Item>>> {
     let subs = admin
         .platform_orchestrator()
         .await
@@ -20,16 +51,36 @@ pub(crate) async fn load_items(
         .await
         .context("Couldn't fetch the subnet orchestrator")?;
 
-    let mut users = stream::iter(subs)
-        .then(|sub| async move { admin.user_index_with(sub).await })
-        .then(|index| async move { index.get_user_canister_list().await })
+    let admin_for_index = admin.clone();
+    let admin_for_individual_user = admin.clone();
+    let items = stream::iter(subs)
+        .then(move |sub| {
+            let admin = admin_for_index.clone();
+            async move {
+                admin
+                    .user_index_with(sub)
+                    .await
+                    .get_user_canister_list()
+                    .await
+            }
+        })
         .and_then(|list| future::ok(stream::iter(list).map(anyhow::Ok)))
         .try_flatten()
-        .and_then(|user_principal| async move {
-            anyhow::Ok(admin.individual_user_for(user_principal).await)
+        .and_then(move |user_principal| {
+            let admin = admin_for_individual_user.clone();
+            async move {
+                let index = admin.individual_user_for(user_principal).await;
+                load_all_posts(&index).await
+            }
+        })
+        .and_then(|list| future::ok(stream::iter(list).map(anyhow::Ok)))
+        .try_flatten()
+        .map(|post| {
+            post.map(|post| Item {
+                video_id: post.video_uid,
+                publisher_user_id: post.created_by_user_principal_id.to_text(),
+            })
         });
 
-    // TODO: iterate over each user and grab posts using the paginated getter
-
-    Ok(stream::once(async { Item::default() }))
+    Ok(items)
 }
