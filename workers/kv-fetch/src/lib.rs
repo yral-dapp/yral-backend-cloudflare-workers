0234946b33002e555db70b3ff39b5660939ba9e4
vishnu <vishnu@gobazzinga.io>
2025-02-05 15:04:25 +0530

    Change authentication to use public provate key pair and jwt token

--- workers/kv-fetch/src/lib.rs
+++ workers/kv-fetch/src/lib.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use serde::Deserialize;
 use serde_json::json;
 use worker::*;
 
@@ -9,21 +13,8 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     router
         .post_async("/:key", |mut req, ctx| async move {
-            let req_auth_key = match req.headers().get("Authorization") {
-                Err(err) => return Response::error(format!("invalid header: {}", err), 401),
-                Ok(None) => return Response::error("unauthorized", 400),
-                Ok(Some(key)) => key,
-            };
-
-            let auth_key = match ctx.secret("SET_VALUE_API_KEY") {
-                Err(err) => {
-                    return Response::error(format!("error fetching auth key: {}", err), 500)
-                }
-                Ok(auth_key) => auth_key.to_string(),
-            };
-
-            if req_auth_key != auth_key {
-                return Response::error("unauthorized", 400);
+            if let Err((msg, status)) = verify_jwt_token(&req, &ctx) {
+                return Response::error(msg, status);
             }
 
             let Some(key) = ctx.param("key") else {
@@ -50,7 +41,11 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
                 Err(err) => Response::error(format!("unable to set value: {}", err), 500),
             }
         })
-        .get_async("/:key", |_req, ctx| async move {
+        .get_async("/:key", |req, ctx| async move {
+            if let Err((msg, status)) = verify_jwt_token(&req, &ctx) {
+                return Response::error(msg, status);
+            }
+
             let Some(key) = ctx.param("key") else {
                 return Response::error("expected key path param", 400);
             };
@@ -62,4 +57,55 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         })
         .run(req, env)
         .await
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct TokenClaims {
+    pub sub: String,
+    pub company: String,
+}
+
+fn verify_jwt_token(
+    req: &Request,
+    ctx: &RouteContext<()>,
+) -> std::result::Result<(), (String, u16)> {
+    let public_key = match ctx.secret("PUBLIC_KEY") {
+        Err(err) => {
+            return Err((format!("unable to fetch public key: {}", err), 500));
+        }
+        Ok(auth_key) => auth_key.to_string(),
+    };
+
+    let public_key = format!(
+        "-----BEGIN PUBLIC KEY-----\n{}\n-----END PUBLIC KEY-----",
+        public_key
+    );
+
+    let public_key = match DecodingKey::from_ed_pem(public_key.as_bytes()) {
+        Err(err) => return Err((format!("unable to parse public key: {}", err), 500)),
+        Ok(key) => key,
+    };
+
+    let mut validation = Validation::new(Algorithm::EdDSA);
+    validation.required_spec_claims = HashSet::new();
+    validation.validate_exp = false;
+
+    let req_token = match req.headers().get("Authorization") {
+        Err(err) => return Err((format!("invalid header: {}", err), 401)),
+        Ok(None) => return Err(("unauthorized".into(), 401)),
+        Ok(Some(key)) => key.replace("Bearer ", ""),
+    };
+
+    let req_token = match decode::<TokenClaims>(&req_token, &public_key, &validation) {
+        Err(err) => return Err((format!("failed to decode token: {}", err), 401)),
+        Ok(req_token) => req_token,
+    };
+
+    let claims = req_token.claims;
+
+    if claims.sub != "hot-or-not-web-leptos-ssr" || claims.company != "gobazzinga" {
+        return Err(("unauthorized".into(), 401));
+    }
+
+    Ok(())
 }
