4acd36e97dfcf66b266746ec33a007e9cf5ab6ca
tushar-saxena-yral <tushar@gobazzinga.io>
2025-03-04 00:03:47 +0530

    feat: add idempotency and metrics

--- workers/storj-bulk-upload/src/lib.rs
+++ workers/storj-bulk-upload/src/lib.rs
@@ -1,17 +1,22 @@
 mod admin;
 mod posts;
 
-use std::sync::{
-    atomic::{AtomicU64, Ordering},
-    Arc,
+use std::{
+    collections::BTreeMap,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
 };
 
 use admin::AdminCanisters;
 use anyhow::Context;
 use chrono::{DateTime, Utc};
 use futures::TryStreamExt;
+use serde_json::json;
 use worker::{
-    console_error, event, query, Context as WorkerContext, Env, Request, Response, Result,
+    console_error, console_log, console_warn, event, query, Context as WorkerContext, D1Database,
+    Env, Request, Response, Result,
 };
 
 #[event(start)]
@@ -19,10 +24,30 @@ fn start() {
     console_error_panic_hook::set_once();
 }
 
+async fn get_item_count_in_staging(work_items: &D1Database) -> Result<usize> {
+    let count = query!(
+        &work_items,
+        "SELECT COUNT(video_id) as count from work_items"
+    )
+    .all()
+    .await?;
+    assert!(count.success(), "query to succeed");
+
+    // quick and dirty
+    let count = count.results::<BTreeMap<String, usize>>()?;
+    let count = count.first().unwrap().get("count").unwrap();
+
+    Ok(*count)
+}
+
 #[event(fetch, respond_with_errors)]
 async fn fetch(_req: Request, env: Env, _ctx: WorkerContext) -> Result<Response> {
     let admin = AdminCanisters::new(AdminCanisters::get_identity());
     let work_items = env.d1("STORJ_STAGING_DB")?;
+    let count = get_item_count_in_staging(&work_items).await?;
+
+    console_log!("Starting out with {count} items in the d1 staging db");
+
     let low_pass = env
         .var("LOW_PASS_TIMESTAMP")?
         .to_string()
@@ -47,28 +72,44 @@ async fn fetch(_req: Request, env: Env, _ctx: WorkerContext) -> Result<Response>
         }
     };
 
-    let total = AtomicU64::new(0);
+    let added = AtomicU64::new(0);
+    let skipped = AtomicU64::new(0);
+    let unknown = AtomicU64::new(0);
 
     // the operation is io bound, so this number can be optimized to saturate
     // the network of the machine running the worker
     const CONCURRENCY_FACTOR: usize = 100;
     let res = item_stream
         .try_for_each_concurrent(CONCURRENCY_FACTOR, |item| {
-            let total = &total;
+            let added = &added;
+            let skipped = &skipped;
+            let unknown = &unknown;
             let work_items = &work_items;
             async move {
                 let q = query!(
                     work_items,
-                    "INSERT INTO work_items (post_id, video_id, publisher_user_id) VALUES (?1, ?2, ?3)",
+                    "INSERT OR IGNORE INTO work_items (post_id, video_id, publisher_user_id) VALUES (?1, ?2, ?3)",
                     &item.post_id,
                     &item.video_id,
                     &item.publisher_user_id,
                 )
                 .inspect_err(|err| console_error!("Couldn't prepare statement: {err}"))?;
-                q.run()
+                let data = q.run()
                     .await
                     .inspect_err(|err| console_error!("Couldn't insert into db: {err}"))?;
-                total.fetch_add(1, Ordering::Relaxed);
+                match data.meta()?.and_then(|meta| meta.changed_db) {
+                    Some(true) => {
+                        added.fetch_add(1, Ordering::Relaxed);
+                    },
+                    Some(false) => {
+                        skipped.fetch_add(1, Ordering::Relaxed);
+                    },
+                    None => {
+                        // not too sure when this will come, so lets keep it as additional metric
+                        // expect it to be zero, but if its none zero, it must be investigated
+                        unknown.fetch_add(1, Ordering::Relaxed);
+                    },
+                }
                 anyhow::Ok(())
             }
         })
@@ -80,8 +121,13 @@ async fn fetch(_req: Request, env: Env, _ctx: WorkerContext) -> Result<Response>
         return Response::error("Failed to load items", 500);
     }
 
-    Response::ok(format!(
-        "A total of {} posts were added to item store",
-        total.load(Ordering::SeqCst)
-    ))
+    Response::from_json(&json!({
+        "added": added.load(Ordering::SeqCst),
+        "skipped": skipped.load(Ordering::SeqCst),
+        "unknown": unknown.load(Ordering::SeqCst),
+        "total": {
+            "before": count,
+            "after": get_item_count_in_staging(&work_items).await?
+        }
+    }))
 }
