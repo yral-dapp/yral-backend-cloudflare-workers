1c0e9079faee2d5e5d84e2feb419590ed3d48981
rupansh <rupanshsekar@hotmail.com>
2025-04-20 19:59:09 +0530

    fix(pumpdump): add audience in jwt verification

--- workers/yral-pump-n-dump/src/jwt-gen.rs
+++ workers/yral-pump-n-dump/src/jwt-gen.rs
@@ -1,9 +1,12 @@
+mod jwt;
+
 use jsonwebtoken::{get_current_timestamp, EncodingKey, Header};
 use serde::{Deserialize, Serialize};
 use std::fs;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
+    pub aud: String,
     pub exp: usize,
 }
 
@@ -15,11 +18,14 @@ fn main() {
     // 180 days
     let expiry = get_current_timestamp() + (180 * 24 * 60 * 60);
 
-    let claims = Claims {
+    let claims = jwt::Claims {
+        aud: "pump-n-dump-worker".to_string(),
         exp: expiry as usize,
     };
 
     let token = jsonwebtoken::encode(&header, &claims, &enc_key).expect("failed to encode JWT");
+    jwt::verify_jwt(&token).unwrap();
+
     println!("JWT with 180 days expiry:");
     println!("{token}");
 }
--- workers/yral-pump-n-dump/src/jwt.rs
+++ workers/yral-pump-n-dump/src/jwt.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use jsonwebtoken::DecodingKey;
 use serde::{Deserialize, Serialize};
 
@@ -7,14 +9,19 @@ MCowBQYDK2VwAyEAV+DJfztWOovpmCUcZ5Fram2BLOt2B4LIlzw2vogIqK4=
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
+    pub aud: String,
     pub exp: usize,
 }
 
 pub fn verify_jwt(jwt: &str) -> Result<(), jsonwebtoken::errors::Error> {
+    let mut validation = jsonwebtoken::Validation::default();
+    validation.aud = Some(HashSet::from([String::from("pump-n-dump-worker")]));
+    validation.algorithms = vec![jsonwebtoken::Algorithm::EdDSA];
+
     jsonwebtoken::decode::<Claims>(
         jwt,
         &DecodingKey::from_ed_pem(JWT_PUBKEY.as_bytes()).unwrap(),
-        &Default::default(),
+        &validation,
     )?;
 
     Ok(())
